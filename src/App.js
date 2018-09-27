5d7e073cdd001c48af116b7a6dccd57c8770cf02
talves <tony@3alves.com>
2018-09-27 14:37:05 -0700

    vars in netlify.toml

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ class App extends Component {
         <p>NODE_ENV value is "{process.env.NODE_ENV}"</p>
         <p>CUSTOM_ENV_VAR value is "{process.env.CUSTOM_ENV_VAR}"</p>
         <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
+        <p>TOML_ENV_VAR value is "{process.env.TOML_ENV_VAR}"</p>
+        <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV_VAR}"</p>
       </div>
     );
   }
